24ad63b14dc8f2f9dfd15451826015790c2b2eef
hwlcode <hwlcode@163.com>
2019-09-18 01:32:03 +0800

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'health', loadChildren: './health/health.module#HealthPageModule' },
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
